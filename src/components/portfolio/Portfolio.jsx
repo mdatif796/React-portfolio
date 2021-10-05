562ebef5199f76ffd30c701482e8da041a04e193
mdatif796 <mdatif796@gmail.com>
2021-10-05 20:16:57 +0530

    removing some error from console

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -66,12 +66,13 @@ function Portfolio() {
             active={selected === item.id}
             setSelected={setSelected}
             id={item.id}
+            key={item.id}
           />
         ))}
       </ul>
       <div className="container">
         {data.map((d) => (
-          <div className="item">
+          <div className="item" key={d.id}>
             <img src={d.img} alt="" />
             <h3>{d.title}</h3>
           </div>
--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -38,7 +38,7 @@ function Works() {
         style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
       >
         {data.map((d) => (
-          <div className="container">
+          <div className="container" key={d.id}>
             <div className="item">
               <div className="left">
                 <div className="leftContainer">
